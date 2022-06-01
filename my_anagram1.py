224ff2b15ddb6b1a81ce4833fa8e0e3de143952f
obioma chijindu <ezekielobiomachi1@gmail.com>
2022-06-01 23:17:17 +0100

    second commit

--- my_anagram1.py
+++ my_anagram1.py
@@ -8,11 +8,17 @@ def find_anagram():
     user_input = input("please enter a word: ")
     user_input2 = input("please enter a second word: ")
     
-    if len(user_input) != len(user_input2):
-        return "this is not an anagram"
-    
-    for char in user_input:
-      if char in user_input2:
-         return True
-      else: 
-          return False
+    if len(user_input) == len(user_input2):
+        sorted_1 = sorted(user_input)
+        sorted_2 = sorted(user_input2)
+
+        if sorted_1 == sorted_2:
+            print ("these are anagrams")
+        
+        else:
+            print ("these are not anagrams")
+    else:
+        print("they are not anagrams!")
+
+find_anagram()
+
